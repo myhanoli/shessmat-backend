3b1b0c5c9c990d2c14db6fa4ab0991dfbe4d8f06
hanoli <hanoli.tl@gmail.com>
2025-10-01 13:20:40 -0600

    Se agrega configuracion de Cors

--- src/main/java/com/hanoli/demojwt/Config/SecurityConfig.java
+++ src/main/java/com/hanoli/demojwt/Config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hanoli.demojwt.Config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.hanoli.demojwt.Jwt.JwtAuthenticationFilter;
 
@@ -26,6 +31,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
         return http
+        		 .cors()  // 👉 Esto habilita CORS en el filtro de seguridad
+                 .and()
             .csrf(csrf -> 
                 csrf
                 .disable())
@@ -45,6 +52,20 @@ public class SecurityConfig {
             .build();
             
             
+    }
+    
+    //Configuración CORS específica para permitir desde Vercel
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://shessmat-frontend.vercel.app")); // Solo tu frontend
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // Necesario si usas cookies o Authorization headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
